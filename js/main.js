579ff78ba92a510a24d67b90d26f7c64719a6440
Unaxe <74629449+Unaxe@users.noreply.github.com>
2022-11-04 15:27:00 -0400

    feat(js-part): finishing the whole Lab js part

---
+++ js/main.js
@@ -0,0 +1,61 @@
+var userSubmit = document.getElementById("user-submit");
+userSubmit === null || userSubmit === void 0 ? void 0 : userSubmit.addEventListener("click", userForm);
+var submitCalculation = document.getElementById("submit-calculation");
+submitCalculation === null || submitCalculation === void 0 ? void 0 : submitCalculation.addEventListener("click", calculate);
+function userForm(e) {
+    e.preventDefault();
+    var firstname = document.getElementById("first-name");
+    var lastname = document.getElementById("last-name");
+    var email = document.getElementById("email");
+    var address = document.getElementById("address");
+    var city = document.getElementById("city");
+    var province = document.getElementById("province");
+    var membership = document.getElementsByName("membership");
+    var output = document.getElementById("output");
+    // get the checked membership
+    var checkedMembership = "";
+    for (var i = 0; i < membership.length; i++) {
+        if (membership[i].checked) {
+            checkedMembership = membership[i].value;
+        }
+    }
+    var result = "\n    <span class=\"result-category\">Full Name:</span> <span id=\"name-result\">".concat(firstname.value, " ").concat(lastname.value, "</span><br/>\n    <span class=\"result-category\">Email:</span> <span id=\"email-result\">").concat(email.value, "</span><br/>\n    <span class=\"result-category\">Address:</span> <span id=\"address-result\">").concat(address.value, " ").concat(city.value, ", ").concat(province.value, "</span><br/>\n    <span class=\"result-category\">Membership:</span> <span id=\"membership-result\">").concat(checkedMembership, "</span><br/>\n  ");
+    output.innerHTML = result;
+}
+function calculate(e) {
+    // we won't return false to avoid the refresh, the better way is to use e.preventDefault()
+    e.preventDefault();
+    var numbers = document.getElementById("numbers");
+    // we will not do as says in the assignment, we don't want to have a lot of code in an els method, we will just return if the input is empty
+    if (numbers.value === "") {
+        return;
+    }
+    // Now as we can see, it's much easier to read and understand
+    var numbs = numbers.value.split(" ").map(function (num) { return parseInt(num); }).filter(function (e) { return !isNaN(e); });
+    var action = document.getElementsByName("operation");
+    var checkedAction = "";
+    for (var i = 0; i < action.length; i++) {
+        if (action[i].checked) {
+            checkedAction = action[i].value;
+        }
+    }
+    var result = 0;
+    switch (checkedAction) {
+        case "sum":
+            result = numbs.reduce(function (a, b) { return a + b; }, 0);
+            break;
+        case "average":
+            result = numbs.reduce(function (a, b) { return a + b; }, 0) / numbs.length;
+            break;
+        case "max":
+            result = Math.max.apply(Math, numbs);
+            break;
+        case "min":
+            result = Math.min.apply(Math, numbs);
+            break;
+        default:
+            break;
+    }
+    var resultElement = document.getElementById("result");
+    resultElement.innerText = result.toString();
+}
---
+++ js/main.ts
@@ -0,0 +1,80 @@
+
+const userSubmit = document.getElementById("user-submit") as HTMLButtonElement;
+userSubmit?.addEventListener("click", userForm);
+
+const submitCalculation = document.getElementById("submit-calculation") as HTMLButtonElement;
+submitCalculation?.addEventListener("click", calculate);
+
+function userForm(e:Event) {
+  e.preventDefault();
+  const firstname = document.getElementById("first-name") as HTMLInputElement;
+  const lastname = document.getElementById("last-name") as HTMLInputElement;
+  const email = document.getElementById("email") as HTMLInputElement;
+  const address = document.getElementById("address") as HTMLInputElement;
+  const city = document.getElementById("city") as HTMLInputElement;
+  const province = document.getElementById("province") as HTMLInputElement;
+  const membership = document.getElementsByName(
+    "membership"
+  ) as NodeListOf<HTMLInputElement>;
+
+  const output = document.getElementById("output") as HTMLDivElement;
+  // get the checked membership
+  let checkedMembership = "";
+  for (let i = 0; i < membership.length; i++) {
+    if (membership[i].checked) {
+      checkedMembership = membership[i].value;
+    }
+  }
+  
+  
+  const result = `
+    <span class="result-category">Full Name:</span> <span id="name-result">${firstname.value} ${lastname.value}</span><br/>
+    <span class="result-category">Email:</span> <span id="email-result">${email.value}</span><br/>
+    <span class="result-category">Address:</span> <span id="address-result">${address.value} ${city.value}, ${province.value}</span><br/>
+    <span class="result-category">Membership:</span> <span id="membership-result">${checkedMembership}</span><br/>
+  `;  
+  output.innerHTML = result;
+}
+
+function calculate(e:Event) {
+  // we won't return false to avoid the refresh, the better way is to use e.preventDefault()
+  e.preventDefault();
+    
+  const numbers = document.getElementById("numbers") as HTMLInputElement;
+  // we will not do as says in the assignment, we don't want to have a lot of code in an els method, we will just return if the input is empty
+  if (numbers.value === "") {
+    return;
+  }
+  // Now as we can see, it's much easier to read and understand
+
+  let numbs = numbers.value.split(" ").map((num) => parseInt(num)).filter((e) => !isNaN(e));
+  let action = document.getElementsByName("operation") as NodeListOf<HTMLInputElement>;
+  let checkedAction = "";
+  for (let i = 0; i < action.length; i++) {
+    if (action[i].checked) {
+      checkedAction = action[i].value;
+    }
+  }
+
+  let result = 0;
+  switch (checkedAction) {
+    case "sum":
+      result = numbs.reduce((a, b) => a + b, 0);
+      break;
+    case "average":
+      result = numbs.reduce((a, b) => a + b, 0) / numbs.length;
+      break;
+    case "max":
+      result = Math.max(...numbs);
+      break;
+    case "min":
+      result = Math.min(...numbs);
+      break;
+    default:
+      break;
+  }
+  
+  const resultElement = document.getElementById("result") as HTMLSpanElement;
+
+  resultElement.innerText = result.toString();
+}
